c45a221d3aaa84abac2169a91537c6936db2e9ec
Arignir <tigearth@hotmail.fr>
2022-10-15 17:59:28 +0200

    Rename `emu.rs` to `emulator.rs`.

--- hs-gba/src/lib.rs
+++ hs-gba/src/lib.rs
@@ -1,11 +1,11 @@
 #![allow(dead_code)]
 
 mod core;
-mod emu;
+mod emulator;
 mod gba;
 mod scheduler;
 mod memory;
 mod ppu;
 
-pub use emu::Emulator;
+pub use emulator::Emulator;
 pub use ppu::{SCREEN_WIDTH, SCREEN_HEIGHT};
